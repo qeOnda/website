78813f30c0fb3cf32744c484dff4320e57d5ac2a
Shinsuke  May <s.seanmay@gmail.com>
2021-02-17 20:26:55 +0900

    npm install octokit

--- strapi-blog/api/about/models/about.js
+++ strapi-blog/api/about/models/about.js
@@ -1,8 +1,12 @@
 'use strict';
 
+const axios = require('axios')
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+	
+};
