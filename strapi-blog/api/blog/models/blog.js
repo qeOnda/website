fddee0b94baeefe78667c8af56de51dceea051e1
Shinsuke  May <s.seanmay@gmail.com>
2021-02-18 08:52:43 +0900

    testing model configure for dispatch

--- strapi-blog/api/blog/models/blog.js
+++ strapi-blog/api/blog/models/blog.js
@@ -5,7 +5,7 @@ const octokit = new Octokit({ auth: `${process.env.GITHUB_TOKEN}` });
 module.exports = {
 	lifecycles: {
 		async afterCreate(data) {			
-			await octokit.request('POST /repos/qeOnda/website/dispatches', {
+			await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
 			  owner: 'qeOnda',
 			  repo: 'website',
 			  event_type: 'created'
