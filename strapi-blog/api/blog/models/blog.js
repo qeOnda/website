844c1281468a1e610dae0e6270808c71f3af6820
Shinsuke  May <s.seanmay@gmail.com>
2021-02-17 22:23:24 +0900

    update blog api models

--- strapi-blog/api/blog/models/blog.js
+++ strapi-blog/api/blog/models/blog.js
@@ -1,9 +1,10 @@
 'use strict';
-const octokit = new Octokit({ auth: `${process.env.GITHUB_TOKEN}` });
+const { Octokit } = require("@octokit/core");
 
 module.exports = {
 	lifecycles: {
 		async afterCreate(data) {
+			const octokit = new Octokit({ auth: `${process.env.GITHUB_TOKEN}` });
 			await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
 			  owner: 'qeOnda',
 			  repo: 'hello-world',
