0ee7ed2376cc136741d5a422c0b4032f9f6559c1
Shinsuke  May <s.seanmay@gmail.com>
2021-02-18 17:24:28 +0900

    change models

--- strapi-blog/api/blog/models/blog.js
+++ strapi-blog/api/blog/models/blog.js
@@ -4,15 +4,15 @@ const octokit = new Octokit({ auth: `${process.env.GITHUB_TOKEN}` });
 
 module.exports = {
 	lifecycles: {
-		async afterCreate(data) {			
-			await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+		afterCreate(data) {			
+			octokit.request('POST /repos/{owner}/{repo}/dispatches', {
 			  owner: 'qeOnda',
 			  repo: 'website',
 			  event_type: 'created'
 			})
 		}
-		async afterUpdate(data) {			
-			await octokit.request('POST /repos/{owner}/{repo}/dispatches', {
+		afterUpdate(data) {			
+			octokit.request('POST /repos/{owner}/{repo}/dispatches', {
 			  owner: 'qeOnda',
 			  repo: 'website',
 			  event_type: 'created'
